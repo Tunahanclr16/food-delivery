0e93b5e3631e17b39473f08437d2e014ff5a0a6f
Tunahanclr16 <tunahancelerr16@gmail.com>
2024-06-07 17:44:10 +0300

    finish middle aa

--- middleware.js
+++ middleware.js
@@ -1,36 +1,33 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-  const token = req.cookies.get('token');
-  const sessionToken = req.cookies.get('next-auth.session-token');
-  console.log('Token:', token);
-  console.log('Session Token:', sessionToken);
+  const token = req.cookies.get('next-auth.session-token');
+  const adminToken = req.cookies.get('token');
 
+  console.log('Token:', token);
+  
   if (req.nextUrl.pathname.startsWith('/admin')) {
-    if (!token) {
-      console.log('Admin girişi yapmadı, giriş sayfasına yönlendiriliyor.');
+    if (!adminToken) {
+      console.log('Admin girişi yapmadı, /auth/admin sayfasına yönlendiriliyor.');
       return NextResponse.redirect(new URL('/auth/admin', req.url));
     }
   } else if (req.nextUrl.pathname.startsWith('/profile')) {
-    if (!token && !sessionToken) {
-      console.log('Kullanıcı girişi yapmadı, giriş sayfasına yönlendiriliyor.');
-      return NextResponse.redirect(new URL('/auth/login', req.url));
-    } else if (sessionToken && req.cookies.get('role') !== 'admin') {
-      console.log('Admin değil, profile sayfasına erişemez.');
+    if (!token) {
+      console.log('Kullanıcı girişi yapmadı, /auth/login sayfasına yönlendiriliyor.');
       return NextResponse.redirect(new URL('/auth/login', req.url));
     }
   } else if (req.nextUrl.pathname.startsWith('/auth/admin')) {
-    if (token) {
-      console.log('Admin giriş yapmış, admin sayfasına yönlendiriliyor.');
+    if (!token) {
+      console.log('Admin girişi yapmadı, /auth/admin sayfasına yönlendiriliyor.');
+      return NextResponse.redirect(new URL('/auth/admin', req.url));
+    } else {
+      console.log('Admin giriş yapmış, /admin sayfasına yönlendiriliyor.');
       return NextResponse.redirect(new URL('/admin', req.url));
     }
   } else if (req.nextUrl.pathname.startsWith('/auth/login')) {
-    if (token || sessionToken) {
-      console.log('Kullanıcı giriş yapmış, profile sayfasına yönlendiriliyor.');
+    if (token) {
+      console.log('Kullanıcı giriş yapmış, /profile sayfasına yönlendiriliyor.');
       return NextResponse.redirect(new URL('/profile', req.url));
-    } else if (req.cookies.get('role') === 'admin') {
-      console.log('Admin giriş yapmamış, login sayfasına yönlendiriliyor.');
-      return NextResponse.redirect(new URL('/auth/admin', req.url));
     }
   }
 
